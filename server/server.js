9ad8245ecd1d0fe69f149f8d73bd0d486722b8a0
Fernan de Dios <info@mediagramstudios.com>
2018-03-06 20:35:29 +0800

    added middlewares

--- server/server.js
+++ server/server.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const cookieParser = require('cookie-parser');
 
 const app = express();
-const { port, database } = require('./config/secret');
+const { port, database, secretKey } = require('./config/secret');
 
 // database connection
 mongoose.connect(database).then(() => {
   console.log('Connected to DB');
 }, err => console.log(err));
+
+// middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true })); // read utf-8 encoded
+app.use(morgan('dev'));
+app.use(cookieParser()); // read cookies from browser
+
+// store sessions to db
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: secretKey,
+  store: new MongoStore({ url: database, autoReconnect: true })
+}));
 
 
 app.get('/', (req, res, next) => {
