0c2981ff215e5e53f65fb0fd89ab1d6bcbd191be
Fernan de Dios <info@mediagramstudios.com>
2018-03-12 18:02:22 +0800

    added test keys

--- server/config/secret.js
+++ server/config/secret.js
@@ -1,6 +1,9 @@
 if (process.env.NODE_ENV === 'production') {
 	module.exports = require('./prod'); // export prod keys
 }
-else {
+else if (process.env.NODE_ENV === 'development') {
 	module.exports = require('./dev'); // export dev keys
+}
+else if (process.env.NODE_ENV === 'test') {
+	module.exports = require('./testkeys'); // export test keys
 }
